b7885168ed2c0da63c0e527cb39cb22b15442c74
Edd Gachira <edwingachira@gmail.com>
2023-01-19 19:31:08 +0300

    initialized project

---
+++ furniture_app/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:furniture_app/screens/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Furniture UI App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Color(0xFFE376553),
+        ),
+        primaryColor: Color(0xFFE376553),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ furniture_app/lib/screens/home_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 30.0,
+          vertical: 40.0,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Furniture \nin your style',
+              style: TextStyle(
+                fontSize: 32.0,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Container(
+                  height: 150.0,
+                  width: MediaQuery.of(context).size.width,
+                  // margin: EdgeInsets.only(top: 50.0),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        Text('Elegant\nwhite chari'),
+                        Text('\$ 200'),
+                      ],
+                    ),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Image(
+                        image: AssetImage('assets/images/image_9.png'),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
